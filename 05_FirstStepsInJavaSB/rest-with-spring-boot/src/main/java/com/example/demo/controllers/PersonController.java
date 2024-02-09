0b2ab1e6d78643408776592b6d6a46e8d5f7bb8b
Antonio Neto <absn3@cin.ufpe.br>
2024-02-09 14:27:27 -0300

    adding pagination to person

--- 05_FirstStepsInJavaSB/rest-with-spring-boot/src/main/java/com/example/demo/controllers/PersonController.java
+++ 05_FirstStepsInJavaSB/rest-with-spring-boot/src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.data.vo.v1.PersonVO;
@@ -80,8 +85,16 @@ public class PersonController {
 			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
 		}
 	)
-	public List<PersonVO> findAll() {
-		return service.findAll();
+	public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction
+			) {
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+		
+		return ResponseEntity.ok(service.findAll(pageable));
 	}
 
 	@PostMapping(
